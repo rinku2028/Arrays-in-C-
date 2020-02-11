e8f947a0018f137008ee743cedc3623211d669b2
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-11 23:07:00 +0545

    Get the rank of an array

---
+++ rankofarray.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class rankofarray
+    {
+        public static void Main(string []args)
+        {
+            int[] array1 = new int[5] { 1, 2, 3, 4, 5 };
+            int[,] array2 = new int[10, 3];
+            Console.WriteLine("Total dimensions of Array is : " + array1.Rank);
+            Console.WriteLine("Total dimensions of Array is : " + array2.Rank);
+            Console.ReadLine();
+        }
+    }
+}
