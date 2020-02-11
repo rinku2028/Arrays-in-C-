ae97e52e777aecf9e3cebd3327178049ac19418c
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-11 23:08:57 +0545

    Jagged Arrays

---
+++ jaggedarray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class jaggedarray
+    {
+        public static void Main(string []args)
+        {
+            
+            //Gives jagged arrays
+            int[][] jag = new int[3][];
+            jag[0] = new int[2];
+            jag[0][0] = 11;
+            jag[0][1] = 12;
+            jag[1] = new int[1] { 11 };
+            jag[2] = new int[3] { 14, 15, 16 };
+            for (int i = 0; i < jag.Length; i++)
+            {
+                int[] innerArray = jag[i];
+                for (int a = 0; a < innerArray.Length; a++)
+                {
+                    Console.WriteLine(innerArray[a] + " ");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
